a8b2e331b479ab21d480d43da8035cf7c22fe6c4
Sam Cook <email@sjc.sh>
2024-03-05 15:14:24 -0600

    add GetCallerDir

--- pkg/fsutil/fsutil.go
+++ pkg/fsutil/fsutil.go
@@ -1,7 +1,16 @@
 package fsutil
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
 
 func EnsureDir(path string) error {
 	return os.MkdirAll(path, os.ModePerm)
+}
+
+func GetCallerDir() string {
+	_, file, _, _ := runtime.Caller(0)
+	return filepath.Dir(file)
 }
