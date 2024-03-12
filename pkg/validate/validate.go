b7db3beb1cf9857420dd0329a579eb9e0522582f
Sam Cook <email@sjc.sh>
2024-03-12 13:30:08 -0500

    add UnmarshalFromResponse

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -31,3 +31,13 @@ func (v Validate) UnmarshalFromString(data string, destination interface{}) erro
 	}
 	return nil
 }
+
+func (v Validate) UnmarshalFromResponse(r *http.Response, destination interface{}) error {
+	if err := json.NewDecoder(r.Body).Decode(destination); err != nil {
+		return fmt.Errorf("error decoding JSON: %w", err)
+	}
+	if err := v.Instance.Struct(destination); err != nil {
+		return fmt.Errorf("validation error: %w", err)
+	}
+	return nil
+}
