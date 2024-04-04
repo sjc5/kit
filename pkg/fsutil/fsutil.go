b330eff58e4fe745f94c4e71a088157c7ba48bcb
Sam Cook <email@sjc.sh>
2024-04-04 16:02:55 -0500

    add CopyDir and CopyFile to fsutil

--- pkg/fsutil/fsutil.go
+++ pkg/fsutil/fsutil.go
@@ -1,6 +1,7 @@
 package fsutil
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -13,4 +14,67 @@ func EnsureDir(path string) error {
 func GetCallerDir() string {
 	_, file, _, _ := runtime.Caller(1)
 	return filepath.Dir(file)
+}
+
+// copyDir recursively copies a directory from src to dst.
+func CopyDir(src, dst string) error {
+	// Get properties of source dir
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	// Create the destination directory
+	if err := os.MkdirAll(dst, info.Mode()); err != nil {
+		return err
+	}
+
+	// Read the directory contents
+	entries, err := os.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		fileInfo, err := entry.Info()
+		if err != nil {
+			return err
+		}
+
+		// If the entry is a directory, recurse
+		if fileInfo.IsDir() {
+			if err := CopyDir(srcPath, dstPath); err != nil {
+				return err
+			}
+		} else {
+			// If it's a file, copy it
+			if err := CopyFile(srcPath, dstPath); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// copyFile copies a single file from src to dst
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	if _, err := io.Copy(destFile, sourceFile); err != nil {
+		return err
+	}
+	return destFile.Sync()
 }
