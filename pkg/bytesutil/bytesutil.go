16d7b8fcddd0acc727cff477f4237fa2df74ba18
Sam Cook <email@sjc.sh>
2024-06-20 11:04:52 -0500

    bytesutil updates

--- pkg/bytesutil/bytesutil.go
+++ pkg/bytesutil/bytesutil.go
@@ -1,8 +1,11 @@
 package bytesutil
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/gob"
+	"fmt"
 )
 
 func Random(l int) ([]byte, error) {
@@ -19,4 +22,26 @@ func FromBase64(s string) ([]byte, error) {
 
 func ToBase64(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
+}
+
+func ToGob(src any) ([]byte, error) {
+	var a bytes.Buffer
+	enc := gob.NewEncoder(&a)
+	err := enc.Encode(src)
+	if err != nil {
+		return nil, fmt.Errorf("bytesutil.ToGob: failed to encode src to bytes: %w", err)
+	}
+	return a.Bytes(), nil
+}
+
+func FromGobInto(gobBytes []byte, dest any) error {
+	if gobBytes == nil {
+		return fmt.Errorf("bytesutil.FromGobInto: cannot decode nil bytes")
+	}
+	dec := gob.NewDecoder(bytes.NewReader(gobBytes))
+	err := dec.Decode(dest)
+	if err != nil {
+		return fmt.Errorf("failed to decode bytes into dest: %w", err)
+	}
+	return nil
 }
