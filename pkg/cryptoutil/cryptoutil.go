9857852bc24271bb37c0263ba8409233d7e1b41c
Sam Cook <email@sjc.sh>
2024-06-24 20:00:58 -0500

    update signedcookie and cryptoutil

--- pkg/cryptoutil/cryptoutil.go
+++ pkg/cryptoutil/cryptoutil.go
@@ -7,29 +7,28 @@ import (
 	"golang.org/x/crypto/nacl/sign"
 )
 
-func SignSymmetric(rawMessage []byte, secretBytes *[32]byte) ([]byte, error) {
-	digest := auth.Sum(rawMessage, secretBytes)
-	bytesNeeded := auth.Size + len(rawMessage)
-	encodedValue := make([]byte, bytesNeeded)
-	copy(encodedValue, digest[:])
-	copy(encodedValue[auth.Size:], rawMessage)
-	return encodedValue, nil
+func SignSymmetric(msg []byte, secretKey *[32]byte) ([]byte, error) {
+	digest := auth.Sum(msg, secretKey)
+	signedMsg := make([]byte, auth.Size+len(msg))
+	copy(signedMsg, digest[:])
+	copy(signedMsg[auth.Size:], msg)
+	return signedMsg, nil
 }
 
-func ReadSymmetric(signedMessage []byte, secretBytes *[32]byte) ([]byte, error) {
+func VerifyAndReadSymmetric(signedMsg []byte, secretKey *[32]byte) ([]byte, error) {
 	digest := make([]byte, auth.Size)
-	copy(digest, signedMessage[:auth.Size])
-	rawValue := signedMessage[auth.Size:]
-	if !auth.Verify(digest, rawValue, secretBytes) {
+	copy(digest, signedMsg[:auth.Size])
+	msg := signedMsg[auth.Size:]
+	if !auth.Verify(digest, msg, secretKey) {
 		return nil, errors.New("invalid signature")
 	}
-	return rawValue, nil
+	return msg, nil
 }
 
-func ReadAssymetric(signedMessage []byte, publicKey *[32]byte) ([]byte, error) {
-	message, ok := sign.Open(nil, signedMessage, publicKey)
+func VerifyAndReadAssymetric(signedMsg []byte, publicKey *[32]byte) ([]byte, error) {
+	msg, ok := sign.Open(nil, signedMsg, publicKey)
 	if !ok {
 		return nil, errors.New("invalid signature")
 	}
-	return message, nil
+	return msg, nil
 }
--- pkg/signedcookie/signedcookie.go
+++ pkg/signedcookie/signedcookie.go
@@ -1,13 +1,19 @@
 package signedcookie
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/sjc5/kit/pkg/bytesutil"
 	"github.com/sjc5/kit/pkg/cryptoutil"
 )
+
+////////////////////////////////////////////////////////////////////
+/////// CORE SIGNED COOKIES MANAGER
+////////////////////////////////////////////////////////////////////
 
 const (
 	SecretSize = 32 // SecretSize is the size, in bytes, of a cookie secret.
@@ -85,10 +91,89 @@ func (m Manager) Read(signedValue string) (string, error) {
 		return "", fmt.Errorf("error decoding base64: %v", err)
 	}
 	for _, secret := range m.secretsBytes {
-		value, err := cryptoutil.ReadSymmetric(bytes, &secret)
+		value, err := cryptoutil.VerifyAndReadSymmetric(bytes, &secret)
 		if err == nil {
 			return string(value), nil
 		}
 	}
 	return "", errors.New("cookie not valid")
+}
+
+////////////////////////////////////////////////////////////////////
+/////// SIGNED COOKIE HELPERS
+////////////////////////////////////////////////////////////////////
+
+type SignedCookie[T any] struct {
+	Manager    *Manager
+	TTL        time.Duration
+	BaseCookie *http.Cookie
+}
+
+func (ac *SignedCookie[T]) Set(w http.ResponseWriter, r *http.Request, value *T, baseCookie *http.Cookie) error {
+	dataBytes, err := bytesutil.ToGob(value)
+	if err != nil {
+		return err
+	}
+
+	var baseCookieToUse *http.Cookie
+	if baseCookie != nil {
+		baseCookieToUse = baseCookie
+	} else if ac.BaseCookie != nil {
+		baseCookieToUse = ac.BaseCookie
+	}
+
+	var expires time.Time
+	if ac.TTL != 0 {
+		expires = time.Now().Add(ac.TTL)
+	}
+	cookie := newSecureCookie(ac.BaseCookie.Name, &expires, baseCookieToUse)
+	cookie.Value = base64.StdEncoding.EncodeToString(dataBytes)
+
+	return ac.Manager.Set(w, r, cookie)
+}
+
+func (ac *SignedCookie[T]) Delete(w http.ResponseWriter, r *http.Request) error {
+	cookie := newSecureCookie(ac.BaseCookie.Name, nil, nil)
+	cookie.MaxAge = -1
+
+	return ac.Manager.Set(w, r, cookie)
+}
+
+func (ac *SignedCookie[T]) Get(r *http.Request) (*T, error) {
+	var instance T
+
+	value, err := ac.Manager.Get(r, ac.BaseCookie.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	dataBytes, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		return nil, err
+	}
+
+	err = bytesutil.FromGobInto(dataBytes, &instance)
+	if err != nil {
+		return nil, err
+	}
+
+	return &instance, nil
+}
+
+func newSecureCookie(name string, expires *time.Time, baseCookie *http.Cookie) *http.Cookie {
+	newCookie := http.Cookie{}
+
+	if baseCookie != nil {
+		newCookie = *baseCookie
+	}
+
+	newCookie.Name = name
+	if expires != nil {
+		newCookie.Expires = *expires
+	}
+
+	newCookie.HttpOnly = true
+	newCookie.Secure = true
+
+	return &newCookie
 }
