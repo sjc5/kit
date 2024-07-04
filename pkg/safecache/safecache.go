a89d80630a0bf1a7e31f7d704467a86202be7f12
Sam Cook <email@sjc.sh>
2024-07-04 01:07:21 -0500

    add map to safecache

--- pkg/safecache/safecache.go
+++ pkg/safecache/safecache.go
@@ -9,11 +9,11 @@ type Cache[T any] struct {
 	once          sync.Once
 	mu            sync.RWMutex
 	initFunc      func() (T, error)
-	bypassFunc    *func() bool
+	bypassFunc    func() bool
 	isInitialized bool
 }
 
-func New[T any](initFunc func() (T, error), bypassFunc *func() bool) *Cache[T] {
+func New[T any](initFunc func() (T, error), bypassFunc func() bool) *Cache[T] {
 	return &Cache[T]{
 		initFunc:   initFunc,
 		bypassFunc: bypassFunc,
@@ -22,7 +22,7 @@ func New[T any](initFunc func() (T, error), bypassFunc *func() bool) *Cache[T] {
 
 func (c *Cache[T]) Get() (T, error) {
 	// Bypass if bypassFunc is provided and returns true
-	if c.bypassFunc != nil && (*c.bypassFunc)() {
+	if c.bypassFunc != nil && (c.bypassFunc)() {
 		return c.initFunc()
 	}
 
@@ -54,4 +54,53 @@ func (c *Cache[T]) Get() (T, error) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	return c.val, nil
+}
+
+type mapInitFunc[K any, V any] func(key K) (V, error)
+type mapBypassFunc[K any] func(key K) bool
+type mapToKeyFunc[K any, DK comparable] func(key K) DK
+
+type CacheMap[K any, DK comparable, V any] struct {
+	cache         map[DK]*Cache[V]
+	mu            sync.RWMutex
+	mapInitFunc   mapInitFunc[K, V]
+	mapBypassFunc mapBypassFunc[K]
+	mapToKeyFunc  mapToKeyFunc[K, DK]
+}
+
+func NewMap[K any, DK comparable, V any](initFunc mapInitFunc[K, V], mapToKeyFunc mapToKeyFunc[K, DK], bypassFunc mapBypassFunc[K]) *CacheMap[K, DK, V] {
+	return &CacheMap[K, DK, V]{
+		cache:         make(map[DK]*Cache[V]),
+		mapInitFunc:   initFunc,
+		mapToKeyFunc:  mapToKeyFunc,
+		mapBypassFunc: bypassFunc,
+	}
+}
+
+func (c *CacheMap[K, DK, V]) Get(key K) (V, error) {
+	c.mu.RLock()
+	var derivedKey DK
+	if c.mapToKeyFunc != nil {
+		derivedKey = c.mapToKeyFunc(key)
+	}
+	cache, ok := c.cache[derivedKey]
+	c.mu.RUnlock()
+	if !ok {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		var bypassFunc func() bool
+		if c.mapBypassFunc != nil {
+			bypassFunc = func() bool {
+				return c.mapBypassFunc(key)
+			}
+		}
+		cache = New(
+			func() (V, error) {
+				return c.mapInitFunc(key)
+			},
+			bypassFunc,
+		)
+		c.cache[derivedKey] = cache
+	}
+	return cache.Get()
 }
