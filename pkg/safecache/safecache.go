84eb49a9dad4be0d554bca4be43ad55f98ead49a
Sam Cook <email@sjc.sh>
2024-08-15 01:16:13 -0500

    update safecache pkg

--- pkg/safecache/safecache.go
+++ pkg/safecache/safecache.go
@@ -4,17 +4,17 @@ package safecache
 
 import (
 	"sync"
+
+	"github.com/sjc5/kit/pkg/typed"
 )
 
 // Cache is a generic, thread-safe cache that ensures initialization occurs only
 // once unless bypass is requested.
 type Cache[T any] struct {
-	val           T
-	once          sync.Once
-	mu            sync.RWMutex
-	initFunc      func() (T, error)
-	bypassFunc    func() bool
-	isInitialized bool
+	val        T
+	once       sync.Once
+	initFunc   func() (T, error)
+	bypassFunc func() bool
 }
 
 // New creates a new Cache instance. If bypassFunc is provided and returns true,
@@ -31,33 +31,13 @@ func New[T any](initFunc func() (T, error), bypassFunc func() bool) *Cache[T] {
 
 // Get retrieves the cached value, initializing it if necessary or bypassing the cache.
 func (c *Cache[T]) Get() (T, error) {
-	// If bypassFunc is provided and returns true, always run initFunc
 	if c.bypassFunc != nil && (c.bypassFunc)() {
 		return c.initFunc()
 	}
 
-	// First, try to read without locking
-	c.mu.RLock()
-	if c.isInitialized {
-		defer c.mu.RUnlock()
-		return c.val, nil
-	}
-	c.mu.RUnlock()
-
-	// If not initialized, use sync.Once to ensure single initialization
 	var err error
-	c.once.Do(func() {
-		c.mu.Lock()
-		defer c.mu.Unlock()
-		c.val, err = c.initFunc()
-		if err == nil {
-			c.isInitialized = true
-		}
-	})
+	c.once.Do(func() { c.val, err = c.initFunc() })
 
-	// Return the initialized value
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	return c.val, err
 }
 
@@ -74,8 +54,7 @@ type mapToKeyFunc[K any, DK comparable] func(key K) DK
 // CacheMap is a generic, thread-safe cache map that caches values based on
 // derived keys.
 type CacheMap[K any, DK comparable, V any] struct {
-	cache         map[DK]*Cache[V]
-	mu            sync.RWMutex
+	cache         typed.SyncMap[DK, *Cache[V]]
 	mapInitFunc   mapInitFunc[K, V]
 	mapBypassFunc mapBypassFunc[K]
 	mapToKeyFunc  mapToKeyFunc[K, DK]
@@ -95,7 +74,7 @@ func NewMap[K any, DK comparable, V any](
 		panic("mapToKeyFunc must not be nil")
 	}
 	return &CacheMap[K, DK, V]{
-		cache:         make(map[DK]*Cache[V]),
+		cache:         typed.SyncMap[DK, *Cache[V]]{},
 		mapInitFunc:   initFunc,
 		mapToKeyFunc:  mapToKeyFunc,
 		mapBypassFunc: bypassFunc,
@@ -105,33 +84,14 @@ func NewMap[K any, DK comparable, V any](
 // Get retrieves the cached value for the given key, initializing it if necessary
 // or bypassing the cache.
 func (c *CacheMap[K, DK, V]) Get(key K) (V, error) {
-	derivedKey := c.mapToKeyFunc(key)
-
-	c.mu.RLock()
-	cache, ok := c.cache[derivedKey]
-	c.mu.RUnlock()
-	if !ok {
-		c.mu.Lock()
-		defer c.mu.Unlock()
-
-		// Double-check whether the cache has been created during the lock acquisition
-		if cache, ok = c.cache[derivedKey]; !ok {
-			var bypassFunc func() bool
-			if c.mapBypassFunc != nil {
-				bypassFunc = func() bool {
-					return c.mapBypassFunc(key)
-				}
-			}
-			cache = New(
-				func() (V, error) {
-					return c.mapInitFunc(key)
-				},
-				bypassFunc,
-			)
-			c.cache[derivedKey] = cache
-		}
+	if c.mapBypassFunc != nil && (c.mapBypassFunc)(key) {
+		return c.mapInitFunc(key)
 	}
 
-	// If bypass is requested, the Cache's Get method will handle it appropriately
-	return cache.Get()
+	itemCache, _ := c.cache.LoadOrStore(
+		c.mapToKeyFunc(key),
+		New(func() (V, error) { return c.mapInitFunc(key) }, nil),
+	)
+
+	return itemCache.Get()
 }
