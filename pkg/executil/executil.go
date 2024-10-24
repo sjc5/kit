7e870d6d6d891a8aad563afd6411d30d407fd63c
Sam Cook <email@sjc.sh>
2024-10-23 23:46:53 -0500

    update executil pkg

--- pkg/executil/executil.go
+++ pkg/executil/executil.go
@@ -8,10 +8,12 @@ import (
 )
 
 func MakeCmdRunner(commands ...string) func() error {
-	cmd := exec.Command(commands[0], commands[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run
+	return func() error {
+		cmd := exec.Command(commands[0], commands[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
+	}
 }
 
 func GetExecutableDir() (string, error) {
